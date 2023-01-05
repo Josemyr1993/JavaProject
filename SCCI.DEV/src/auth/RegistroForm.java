131229587718eac44c5554aefab3ef716f38cb95
Josemyr1993 <jose-myr@hotmail.com>
2023-01-05 15:40:41 +0100

    Criação do Formulário de Cadastro Geral admin e endusers

--- SCCI.DEV/src/auth/RegistroForm.java
+++ SCCI.DEV/src/auth/RegistroForm.java
@@ -5,10 +5,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import java.awt.Choice;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JPasswordField;
+import java.awt.Canvas;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class RegistroForm extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -30,12 +51,93 @@ public class RegistroForm extends JFrame {
 	 * Create the frame.
 	 */
 	public RegistroForm() {
+		setBackground(SystemColor.desktop);
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\jsebastiao1\\Desktop\\JavaProject\\JavaProject\\Images\\user.png"));
+		setTitle("Formulario de Cadastro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 551, 546);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
+		contentPane.setBackground(new Color(102, 153, 204));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nome:");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel.setBounds(33, 122, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Utilizador:");
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(33, 195, 75, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Email:");
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(256, 122, 46, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Password");
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_4.setBounds(256, 195, 65, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JList list = new JList();
+		list.setBounds(180, 56, 1, 1);
+		contentPane.add(list);
+		
+		textField = new JTextField();
+		textField.setBounds(33, 147, 157, 25);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(33, 217, 157, 25);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(256, 147, 145, 25);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Categoria:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(33, 264, 65, 18);
+		contentPane.add(lblNewLabel_2);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Admin");
+		buttonGroup.add(rdbtnNewRadioButton);
+		rdbtnNewRadioButton.setBounds(33, 289, 109, 23);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("EndUser");
+		buttonGroup.add(rdbtnNewRadioButton_1);
+		rdbtnNewRadioButton_1.setBounds(33, 311, 109, 23);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(256, 218, 145, 23);
+		contentPane.add(passwordField);
+		
+		JLabel lblNewLabel_5 = new JLabel("Registro de Utilizador");
+		lblNewLabel_5.setForeground(new Color(255, 255, 255));
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_5.setBounds(144, 23, 157, 45);
+		contentPane.add(lblNewLabel_5);
+		
+		JButton btnNewButton = new JButton("Validar");
+		btnNewButton.setBounds(256, 439, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Reset");
+		btnNewButton_1.setBounds(385, 439, 89, 23);
+		contentPane.add(btnNewButton_1);
 	}
-
 }
